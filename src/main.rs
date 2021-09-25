e26960a1fc1ef990da5766f7864d080e09597968
aoki <ringohub@gmail.com>
2021-09-25 22:51:37 +0900

    Add lex functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Error;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum LexErrorKind {
     InvalidChar(char),
@@ -17,7 +19,7 @@ impl LexError {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum TokenKind {
-    /// [0-9]+
+    /// [0-9][0-9]*
     Number(u64),
     /// +
     Plus,
@@ -81,6 +83,103 @@ impl<T> Annot<T> {
     }
 }
 
+fn lex(input: &str) -> Result<Vec<Token>, LexError> {
+    let mut tokens = Vec::new();
+    let input = input.as_bytes();
+    let mut pos = 0;
+
+    macro_rules! lex_a_token {
+        ($lexer:expr) => {{
+            let (tok, p) = $lexer?;
+            tokens.push(tok);
+            pos = p;
+        }};
+    }
+
+    while pos < input.len() {
+        match input[pos] {
+            b'0'..=b'9' => lex_a_token!(lex_number(input, pos)),
+            b'+' => lex_a_token!(lex_plus(input, pos)),
+            b'-' => lex_a_token!(lex_minus(input, pos)),
+            b'*' => lex_a_token!(lex_asterisk(input, pos)),
+            b'/' => lex_a_token!(lex_slash(input, pos)),
+            b'(' => lex_a_token!(lex_lparen(input, pos)),
+            b')' => lex_a_token!(lex_rparlen(input, pos)),
+            b' ' | b'\n' | b'\t' => {
+                let ((), p) = skip_spaces(input, pos)?;
+                pos = p;
+            }
+            b => return Err(LexError::invalid_char(b as char, Loc(pos, pos + 1))),
+        }
+    }
+    Ok(tokens)
+}
+
+fn consume_byte(input: &[u8], pos: usize, b: u8) -> Result<(u8, usize), LexError> {
+    if input.len() < pos {
+        return Err(LexError::eof(Loc(pos, pos)));
+    }
+
+    if input[pos] != b {
+        return Err(LexError::invalid_char(
+            input[pos] as char,
+            Loc(pos, pos + 1),
+        ));
+    }
+    Ok((b, pos + 1))
+}
+
+fn lex_plus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    match consume_byte(input, start, b'+') {
+        Ok((_, end)) => Ok((Token::plus(Loc(start, end)), end)),
+        Err(err) => Err(err),
+    }
+}
+
+fn lex_minus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'-').map(|(_, end)| (Token::minus(Loc(start, end)), end))
+}
+
+fn lex_asterisk(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'*').map(|(_, end)| (Token::asterisk(Loc(start, end)), end))
+}
+
+fn lex_slash(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'/').map(|(_, end)| (Token::slash(Loc(start, end)), end))
+}
+
+fn lex_lparen(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'(').map(|(_, end)| (Token::lparen(Loc(start, end)), end))
+}
+
+fn lex_rparlen(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b')').map(|(_, end)| (Token::rparen(Loc(start, end)), end))
+}
+
+fn lecognize_many(input: &[u8], mut pos: usize, mut f: impl FnMut(u8) -> bool) -> usize {
+    while pos < input.len() && f(input[pos]) {
+        pos += 1;
+    }
+    pos
+}
+
+fn lex_number(input: &[u8], mut pos: usize) -> Result<(Token, usize), LexError> {
+    use std::str::from_utf8;
+
+    let start = pos;
+    while pos < input.len() && b"1234567890".contains(&input[pos]) {
+        pos += 1;
+    }
+    let n = from_utf8(&input[start..pos]).unwrap().parse().unwrap();
+    Ok((Token::number(n, Loc(start, pos)), pos))
+}
+
+fn skip_spaces(input: &[u8], mut pos: usize) -> Result<((), usize), LexError> {
+    while pos < input.len() && b" \n\t".contains(&input[pos]) {
+        pos += 1;
+    }
+    Ok(((), pos))
+}
 fn main() {
     println!("Hello, world!");
 }
